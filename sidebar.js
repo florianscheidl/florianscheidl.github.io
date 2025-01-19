c1797ff19bdb4cbcaf64b580e4d4f2071108238e
Florian Scheidl <florian.scheidl@gmail.com>
2025-01-19 13:56:37 +0100

    Revert "Sidebar"
    This reverts commit 637e7de27acec75d75e9b4db4949021d332a356e.

--- sidebar.js
+++
@@ -1,9 +0,0 @@
-fetch('sidebar.html')
-  .then(response => response.text())
-  .then(data => {
-    const sidebarContainer = document.getElementById('sidebar-container');
-    sidebarContainer.innerHTML = data;
-  })
-  .catch(error => {
-    console.error('Error fetching sidebar:', error);
-  });
